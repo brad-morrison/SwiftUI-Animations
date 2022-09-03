63eee936c9091841ac7354127ebfe61b71f84f51
Bradley Morrison <45197952+brad-morrison@users.noreply.github.com>
2022-09-03 18:46:56 +0100

    Created XCode project

---
+++ Animated/Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Bradley Morrison on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
